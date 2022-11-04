fb82cb09babdb5a066fd05475760bd2d1347dda0
unknown <pguyon69@gmail.com>
2022-11-04 14:34:15 +0100

    add css on active link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "../Styles/header.css";
-import logo from "../Assets/logo-hrnet.svg";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
-      <NavLink to="/">
-        <img src={logo} alt="tree" />
-      </NavLink>
       <nav className="navbar">
         <NavLink
-          className={({ isActive }) => (isActive ? "active" : '')}
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
           to="/create"
         >
           Create Employee
         </NavLink>
         <NavLink
-          className={({ isActive }) => (isActive ? "active" : '')}
+          className={({ isActive }) => (isActive ? "active" : "")}
           to="/employee"
         >
           Employee List
