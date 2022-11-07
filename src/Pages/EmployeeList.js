45488356bf89a9076d1857a71530b75350d11bf0
unknown <pguyon69@gmail.com>
2022-11-07 11:42:40 +0100

    update table

--- src/Pages/EmployeeList.js
+++ src/Pages/EmployeeList.js
@@ -2,59 +2,11 @@ import React, { useMemo } from "react";
 import MaterialReactTable from "material-react-table";
 import { Employee } from "../Mocked/MockedData";
 
-const data = [
-  {
-    name: {
-      firstName: "John",
-      lastName: "Doe",
-    },
-    address: "261 Erdman Ford",
-    city: "East Daphne",
-    state: "Kentucky",
-  },
-  {
-    name: {
-      firstName: "Jane",
-      lastName: "Doe",
-    },
-    address: "769 Dominic Grove",
-    city: "Columbus",
-    state: "Ohio",
-  },
-  {
-    name: {
-      firstName: "Joe",
-      lastName: "Doe",
-    },
-    address: "566 Brakus Inlet",
-    city: "South Linda",
-    state: "West Virginia",
-  },
-  {
-    name: {
-      firstName: "Kevin",
-      lastName: "Vandy",
-    },
-    address: "722 Emie Stream",
-    city: "Lincoln",
-    state: "Nebraska",
-  },
-  {
-    name: {
-      firstName: "Joshua",
-      lastName: "Rolluffs",
-    },
-    address: "32188 Larkin Turnpike",
-    city: "Charleston",
-    state: "South Carolina",
-  },
-];
-
 const EmployeeList = () => {
   const columns = useMemo(
     () => [
       {
-        accessorKey: "firstName", //access nested data with dot notation
+        accessorKey: "firstName",
         header: "First Name",
       },
       {
@@ -67,28 +19,28 @@ const EmployeeList = () => {
       },
       {
         accessorKey: "startDate",
-        header: "Start Date"
+        header: "Start Date",
       },
       {
-        accessorKey: 'departement',
-        header: 'departement'
+        accessorKey: "department",
+        header: "department",
       },
       {
         accessorKey: "street",
-        header: "Street"
+        header: "Street",
       },
       {
         accessorKey: "city",
-        header: 'City'
+        header: "City",
       },
       {
-        accessorKey: 'state',
-        header: 'State'
+        accessorKey: "state",
+        header: "State",
       },
       {
-        accessorKey: 'zipcode',
-        header: 'ZipCode'
-      }
+        accessorKey: "zipCode",
+        header: "ZipCode",
+      },
     ],
     []
   );
