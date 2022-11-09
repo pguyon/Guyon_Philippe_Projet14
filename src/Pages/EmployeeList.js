fc06ee6fb3431930629ecd74ece4e9743e21809e
unknown <pguyon69@gmail.com>
2022-11-09 16:33:52 +0100

    install redux

--- src/Pages/EmployeeList.js
+++ src/Pages/EmployeeList.js
@@ -27,7 +27,7 @@ const EmployeeList = () => {
       },
       {
         accessorKey: "department",
-        header: "department",
+        header: "Department",
       },
       {
         accessorKey: "street",
