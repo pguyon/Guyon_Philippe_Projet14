b0eb23d18dd3414f0d44bb9bf3c2a8368ed8342d
unknown <pguyon69@gmail.com>
2022-11-10 13:45:47 +0100

    finish employeeSlice

--- src/Store/Slice/EmployeeSlice.js
+++ src/Store/Slice/EmployeeSlice.js
@@ -15,5 +15,47 @@ const initialState = {
 export const employeeSlice = createSlice({
   name: "Employee",
   initialState,
-  reducers: {},
+  reducers: {
+    enteredFirstName: (state, action) => {
+      state.firstName = action.payload;
+    },
+    enteredLastName: (state, action) => {
+      state.lastName = action.payload;
+    },
+    enteredDateOfBirth: (state, action) => {
+      state.dateOfBirth = action.payload;
+    },
+    enteredStartDate: (state, action) => {
+      state.startDate = action.payload;
+    },
+    enteredDepartment: (state, action) => {
+      state.department = action.payload;
+    },
+    enteredStreet: (state, action) => {
+      state.street = action.payload;
+    },
+    enteredCity: (state, action) => {
+      state.city = action.payload;
+    },
+    enteredState: (state, action) => {
+      state.state = action.payload;
+    },
+    enteredZipCode: (state, action) => {
+      state.zipCode = action.payload;
+    },
+  },
 });
+
+export const reducer = employeeSlice.reducer;
+
+export const {
+  enteredFirstName,
+  enteredLastName,
+  enteredDateOfBirth,
+  enteredStartDate,
+  enteredDepartment,
+  enteredStreet,
+  enteredCity,
+  enteredState,
+  enteredZipCode,
+} = employeeSlice.actions;
